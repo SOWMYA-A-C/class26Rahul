1e8d5cbfc1105ae33a7eeda81c648b75df735721
SOWMYA-A-C <ssb.soumya@gmail.com>
2020-07-22 17:35:22 +0530

    added the platform conosle

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup(){
     world = engine.world;
 
     platform=new Ground(150,305,300,170)
+    console.log(platform)
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
